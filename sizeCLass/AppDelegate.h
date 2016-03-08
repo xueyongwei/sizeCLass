5e364c80b0010d379c281c42b3006a552f99fdf2
Xueyongwei <eoe_dev@icloud.com>
2016-03-08 09:56:10 +0800

    Initial Commit

---
+++ sizeCLass/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sizeCLass
+//
+//  Created by xueyongwei on 16/3/8.
+//  Copyright © 2016年 xueyongwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
